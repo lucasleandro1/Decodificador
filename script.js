cadd87392794acab0f4c86d820c5c753970e9add
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-01-23 16:50:20 -0300

    add decodificador

---
+++ script.js
@@ -0,0 +1,52 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let titulomsg = document.getElementById("titulo");
+    let paragrafo = document.getElementById("paragrafo");
+    let img = document.getElementById("imagem");
+
+    let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        titulomsg.textContent = "Texto encriptado com sucesso";
+        paragrafo.textContent = "";
+        img.src = "./img/3301561.jpg";
+    } 
+    else {
+        img.src = "./img/2918314.jpg";
+        titulomsg.textContent = "Nenhum texto encontrado";
+        paragrafo.textContent = "Digite algo para encriptar ou desencriptar";
+        alert("Digite algo!");
+    }
+}
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let titulomsg = document.getElementById("titulo");
+    let paragrafo = document.getElementById("paragrafo");
+    let img = document.getElementById("imagem");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        titulomsg.textContent = "Texto desencriptado com sucesso";
+        paragrafo.textContent = "";
+        img.src = "./img/3301561.jpg";
+    } 
+    else {
+        img.src = "./img/2918314.jpg";
+        titulomsg.textContent = "Nenhum texto encontrado";
+        paragrafo.textContent = "Digite algo para encriptar ou desencriptar";
+        alert("Digite algo!");
+    }
+}
