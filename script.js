b15fcb38f51f6adc1c51809530aef439a32d42fd
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-06-06 21:29:48 -0300

    add ?

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ function encriptar(){
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
+    .replace(/u/gi, "ufat")
+    .replace(/\?/gi, "Qualquer coisa");
 
     if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         titulomsg.textContent = "Texto encriptado com sucesso";
         paragrafo.textContent = "";
         img.src = "./img/3301561.jpg";
-    } 
+    }
     else {
         img.src = "./img/2918314.jpg";
         titulomsg.textContent = "Nenhum texto encontrado";
@@ -35,14 +36,15 @@ function desencriptar(){
     .replace(/imes/gi, "i")
     .replace(/ai/gi, "a")
     .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
+    .replace(/ufat/gi, "u")
+    .replace(/Qualquer coisa/gi, "?");
 
     if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         titulomsg.textContent = "Texto desencriptado com sucesso";
         paragrafo.textContent = "";
         img.src = "./img/3301561.jpg";
-    } 
+    }
     else {
         img.src = "./img/2918314.jpg";
         titulomsg.textContent = "Nenhum texto encontrado";
